dcc9a956cf028058e92c9dfd20308b4582c9af78
ramyar yeganih <ramyaryganh@gmail.com>
2026-08-12 03:06:24 +0330

    version 4

--- project_1/src/Header.jsx
+++ project_1/src/Header.jsx
@@ -1,12 +1,26 @@
+import './Header.css'
+import searchlcon from "../search.png"
+
+
 function Header({ search, setSearch }) {
+
   return (
     <header>
+
+        <div className="search-box">
+          <img className='search-icon' src={searchlcon} alt="search" />
       <input
         type="text"
         placeholder="Search products..."
         value={search}
         onChange={(e) => setSearch(e.target.value)}
       />
+        </div>
+
+        <div>
+        <h1>shop</h1>
+      </div>
+
     </header>
   );
 }
